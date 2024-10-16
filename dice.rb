e90bc2b6a4dbf7016730dac0ca66471fbe44433c
JuliaCraig03 <juliacraig@uchicago.edu>
2024-10-16 16:34:56 +0000

    Sinatra First Web App

---
+++ dice.rb
@@ -0,0 +1,54 @@
+require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+get("/") do
+  "Hello World"
+  <<-HTML
+  <h1>Dice Roll</h1>
+  <ul>
+    <li><a href="/zebra">Zebra Page</a></li>
+    <li><a href="/giraffe">Giraffe Page</a></li>
+    <li><a href="/dice/2/6">Roll 2d6</a></li>
+    <li><a href="/dice/2/10">Roll 2d10</a></li>
+    <li><a href="/dice/1/20">Roll 1d20</a></li>
+  </ul>
+  HTML
+end
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+get("/dice/2/10") do
+first_die = rand(1..10)
+second_die = rand(1..10)
+sum = first_die + second_die
+
+outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+"<h1>2d10</h1>
+<p>#{outcome}</p>"
+end
+get("/dice/1/20") do
+  first_die = rand(1..20)
+  sum = first_die
+
+  outcome = "You rolled a #{first_die} for a total of #{sum}."
+  "<h1>1d20</h1>
+<p>#{outcome}</p>"
+end
